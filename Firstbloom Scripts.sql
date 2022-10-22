d89596dd08e3cf01251369143989cc34fe946c6a
Sean Winikoff <104329741+Seanwinikoff@users.noreply.github.com>
2022-10-21 21:03:07 -0400

    Update Firstbloom Scripts.sql

--- Firstbloom Scripts.sql
+++ Firstbloom Scripts.sql
@@ -1,4 +1,6 @@
---countries and their cities/region ids
+-- creates view of countries and their cities/region ids
+create view coffee_region_ids
+as 
 select product_release_varieties.coffee_region_id, 
 	coffee_regions.city_id, 
 	cities."name" as city, 
@@ -46,20 +48,25 @@ from tasting_notes tn
 order by id desc
 limit 10
 
+
 ------------------------------------------------------------------------------------------------------
 --products with top 10 notes 
-with top_taste_categories as (
-		select sum(cpptc.count), cpptc.tasting_note_id, tasting_notes."name"
+with top_taste_notes as (
+		select sum(cpptc.count), 
+			cpptc.tasting_note_id, tasting_notes."name"
 		from compiled_purchaseable_product_taste_categories cpptc  
-		join tasting_notes on cpptc.tasting_note_id = tasting_notes.id 
-		group by cpptc.tasting_note_id, tasting_notes."name"  
-		order by sum desc
+		join tasting_notes 
+			on cpptc.tasting_note_id = tasting_notes.id 
+		group by cpptc.tasting_note_id, 
+			tasting_notes."name"  
+		order by 
+			sum desc
 		limit 10) 
-select cpptc.tasting_note_id, tasting_notes.name, cpptc.product_id, products.product_name  
+select cpptc.tasting_note_id, tn.name tasting_note, cpptc.product_id, p.product_name  
 from compiled_purchaseable_product_taste_categories cpptc
-join products on cpptc.product_id = products.id 
-join tasting_notes on cpptc.tasting_note_id = tasting_notes.id
-where cpptc.tasting_note_id in (select cpptc.tasting_note_id from top_taste_categories)  
+join products p on cpptc.product_id = p.id 
+join tasting_notes tn on cpptc.tasting_note_id = tn.id
+where cpptc.tasting_note_id in (select cpptc.tasting_note_id from top_taste_notes)  
 order by cpptc.product_id  desc  
 limit 10
 
@@ -73,44 +80,25 @@ with top_tasting_notes as (
 		group by cppsltb.tasting_note_id, tasting_notes."name"  
 		order by sum desc
 		)
-select ccsltb.tasting_note_id, tn.name as varietal, sum(ccsltb.count),  ccsltb.country_id, co.name as country  
+select ccsltb.tasting_note_id, tn.name as tasting_note, sum(ccsltb.count),  ccsltb.country_id, co.name as country  
 from compiled_country_second_level_taste_breakdowns ccsltb 
 join countries co on ccsltb.country_id = co.id
 join tasting_notes tn on ccsltb.tasting_note_id = tn.id  
 where ccsltb.tasting_note_id in (select ccsltb.tasting_note_id from top_tasting_notes)
-group by ccsltb.tasting_note_id,ccsltb.country_id, co."name", tn."name"  
-order by country asc 
+group by ccsltb.tasting_note_id,ccsltb.country_id, co.name, tn.name  
+order by country asc, sum(ccsltb.count) desc  
 
+--try to refine this even more by checking top notes/region
 --------------------------------------------------------------------------------------------------------
 --find each region/country's best varietals
--- select count of times a varietal comes up in a region (use its id), varietal name  
-select product_release_varieties.coffee_region_id, 
-	count(product_release_varieties.variety_id) over (partition by product_release_varieties.variety_id), 
-	--product_release_varieties.variety_id,
-	varieties."name",
-	--coffee_regions.city_id, 
-	cities."name" as city, 
-	--coffee_regions.country_id, 
-	countries."name" as country
-from product_release_varieties 
-join coffee_regions 
-	on product_release_varieties.coffee_region_id  = coffee_regions.id
-join varieties 
-	on product_release_varieties.variety_id = varieties.id 
-join cities 
-	on coffee_regions.city_id = cities.id
-join countries 
-	on coffee_regions.country_id = countries.id
---where 
-group by product_release_varieties.coffee_region_id, 
-	coffee_regions.city_id,
-	product_release_varieties.variety_id,
-	varieties."name",
-	cities."name", 
-	coffee_regions.country_id, 
-	countries."name"
-order by country, 
-	count desc  
+select count(prv.variety_id) over (partition by prv.variety_id), lower(v.name), ci.name as city, co.name as country
+from product_release_varieties prv
+join coffee_regions cr on prv.coffee_region_id  = cr.id
+join varieties v on prv.variety_id = v.id 
+join cities ci on cr.city_id = ci.id
+join countries co on cr.country_id = co.id
+group by prv.coffee_region_id, cr.city_id, prv.variety_id, v."name", ci."name", cr.country_id, co."name"
+order by country, city, count desc 
 
 ----------------------------------------------------------------------------------------------------------
 --counting the different roasters in different countries
@@ -138,8 +126,7 @@ with top_varietals as (
 	group by product_release_varieties.variety_id, varieties.name 
 	order by count desc 
 	limit 10)
-select products.product_name, prv.variety_id,
-	varieties.name
+select products.product_name, prv.variety_id, varieties.name
 from product_release_varieties prv 
 left join products on prv.id = products.id 
 left join product_releases on prv.id = product_releases.product_id  
@@ -147,25 +134,3 @@ left join varieties on prv.variety_id = varieties.id
 where prv.variety_id  in (select prv.producer_id from top_varietals)
 
 --------------------------------------------------------------------------------------------------------
---how much did i spend on FB? (check order_items)
-select oi.order_id, sum(oi.price_paid)
-from order_items oi 
-join orders o on oi.order_id = o.id 
-where order_id = 3
-group by oi.order_id
-
-select customer_name, session_id 
-from orders o 
-where customer_name ='Sean Winikoff'
-
-----------------------------------------------------------------------------------------------------
---look through the reviews as a way to gauge popularity in flavour profiles across coffees 
---with top_ratings as (
-	--select fbupr.rating, fbupr.product_id, p.product_name 
-	--from first_bloom_user_product_reviews fbupr 
-	--join products p on fbupr.product_id = p.id 
-	--order by rating desc
-	--)
---select 
-
-----------------------------------------------------------------------------------------------------
